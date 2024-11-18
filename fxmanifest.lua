f4f0193264b5f2cef54e2eea88e0fee26876977e
Atoshi <atoshit@protonmail.com>
2024-11-18 03:34:50 +0100

    Logger

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,14 @@
+-- Naara Gamemode
 
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
+
+author 'Atoshi'
+description 'Naara PvP'
+version '1.0.0'
+
+shared_scripts {
+    'shared/utils/logger.lua',
+}
