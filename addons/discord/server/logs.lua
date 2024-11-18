faa163a5799358a545129f230657bb17ebe1fd3f
Atoshi <atoshit@protonmail.com>
2024-11-18 10:46:33 +0100

    Discord Logs

---
+++ addons/discord/server/logs.lua
@@ -0,0 +1,44 @@
+local webhookQueue = {}
+local isProcessingWebhooks = false
+
+---@param webhook string: The webhook URL to send the log to
+---@param content table: The content of the log
+local function send(webhook, content)
+    PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({embeds = {
+        {
+            ['title'] = content.title or "Title not defined",
+            ['description'] = content.message or "Message not defined",
+            ['color'] = content.color or 16776960,
+            ["footer"] = {
+                ["text"] = content.footer or "Footer not defined",
+            },
+            ["timestamp"] = os.date("!%Y-%m-%dT%H:%M:%SZ"),
+            ["thumbnail"] = { 
+                ["url"] = content.image or GetConvar("naara:serverIcon", "")
+            }
+        }
+    }}), {['Content-Type'] = 'application/json'})
+end
+
+local function processWebhookQueue()
+    isProcessingWebhooks = true
+
+    CreateThread(function()
+        while #webhookQueue > 0 do
+            local webhookData = table.remove(webhookQueue, 1)
+            send(webhookData.webhook, webhookData.content)
+            Wait(1000)
+        end
+        isProcessingWebhooks = false
+    end)
+end
+
+---@param webhook string: The webhook URL to send the log to
+---@param content table: The content of the log
+function discordLog(webhook, content)
+    table.insert(webhookQueue, {webhook = webhook, content = content})
+
+    if not isProcessingWebhooks then
+        processWebhookQueue()
+    end
+end
--- server/onResourceStart.lua
+++ server/onResourceStart.lua
@@ -10,4 +10,22 @@ AddEventHandler("onResourceStart", function(resourceName)
     end
 
     print("^2Naara Gamemode has been started^7")
+
+    if GetConvar("naara:webhook:gamemodeStart", "") ~= "" then
+        discordLog(GetConvar("naara:webhook:gamemodeStart", ""), {
+            title = "Gamemode Started",
+            message = 
+                'Server Name ```' .. GetConvar("naara:serverName", "Unknown") ..'```\n'..
+                'Debug Level ```' .. GetConvar("naara:debugLevel", "Unknown") ..'```\n'..
+                'Resource Name ```' .. resourceName ..'```\n'..
+                'Resources Count ```' .. GetNumResources() ..'```\n'..
+                'Ped Count ```' .. #GetAllPeds() ..'```\n'..
+                'Vehicle Count ```' .. #GetAllVehicles() ..'```\n'..
+                'Object Count ```' .. #GetAllObjects() ..'```\n'..
+                'Memory Usage ```' .. math.floor(collectgarbage("count")) ..' KB```\n',
+            color = 16753920,
+            footer = "made by Atoshi",
+            image = GetConvar("naara:serverIcon", "")
+        })
+    end
 end)
